43e077c2110ce3e510e39610623d7d8b1da81652
haido <huuhai@jv-it.com.vn>
2015-06-10 08:34:39 +0700

    password: cancel

---
+++ backend/assets/AppAsset.php
@@ -0,0 +1,70 @@
+<?php
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all',
+        'css/site.css',
+        'css/global/plugins/font-awesome/css/font-awesome.min.css',
+        'css/global/plugins/simple-line-icons/simple-line-icons.min.css',
+        'css/global/plugins/bootstrap/css/bootstrap.min.css',
+        'css/global/plugins/uniform/css/uniform.default.css',
+        'css/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css',
+        'css/global/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css',
+        'css/global/plugins/fullcalendar/fullcalendar.min.css',
+        'css/global/plugins/jqvmap/jqvmap/jqvmap.css',
+        'css/admin/pages/css/tasks.css',
+        'css/global/css/components.css',
+        'css/global/css/plugins.css',
+        'css/admin/layout/css/layout.css',
+        'css/admin/layout/css/themes/darkblue.css',
+        'css/admin/layout/css/custom.css',
+
+    ];
+    public $js = [
+        'css/global/plugins/jquery.min.js',
+        'css/global/plugins/jquery-migrate.min.js',
+        'css/global/plugins/jquery-ui/jquery-ui.min.js',
+        'css/global/plugins/bootstrap/js/bootstrap.min.js',
+        'css/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js',
+        'css/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js',
+        'css/global/plugins/jquery.blockui.min.js',
+        'css/global/plugins/jquery.cokie.min.js',
+        'css/global/plugins/uniform/jquery.uniform.min.js',
+        'css/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
+        'css/global/plugins/jqvmap/jqvmap/jquery.vmap.js',
+        'css/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js',
+        'css/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js',
+        'css/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js',
+        'css/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js',
+        'css/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js',
+        'css/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js',
+        'css/global/plugins/flot/jquery.flot.min.js',
+        'css/global/plugins/flot/jquery.flot.resize.min.js',
+        'css/global/plugins/flot/jquery.flot.categories.min.js',
+        'css/global/plugins/jquery.pulsate.min.js',
+        'css/global/plugins/bootstrap-daterangepicker/moment.min.js',
+        'css/global/plugins/bootstrap-daterangepicker/daterangepicker.js',
+        'css/global/plugins/fullcalendar/fullcalendar.min.js',
+        'css/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js',
+        'css/global/plugins/jquery.sparkline.min.js',
+        'css/global/scripts/metronic.js',
+        'css/admin/layout/scripts/layout.js',
+        'css/admin/layout/scripts/quick-sidebar.js',
+        'css/admin/layout/scripts/demo.js',
+        'css/admin/pages/scripts/index.js',
+        'css/admin/pages/scripts/tasks.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+    public $jsOptions = [
+        'position' => \yii\web\View::POS_HEAD
+    ];
+}
---
+++ backend/views/layouts/main.php
@@ -0,0 +1,94 @@
+<?php
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <?= Html::csrfMetaTags() ?>
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => 'JV-IT',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => 'Home', 'url' => ['/site/index']],
+    ];
+
+    $menuItems[] =
+        ['label' => 'Manage account', 'url' => ['/site/manageAccount']];
+    $menuItems[] = ['label' => 'Quản lý bài viết', 'url' => ['/site/manageblog']];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } else {
+        $menuItems[] = [
+            'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+            'url' => ['/site/logout'],
+            'linkOptions' => ['data-method' => 'post']
+        ];
+    }
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; JV-IT <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+        Metronic.init(); // init metronic core componets
+        Layout.init(); // init layout
+        QuickSidebar.init(); // init quick sidebar
+        Demo.init(); // init demo features
+        Index.init();
+        Index.initDashboardDaterange();
+        Index.initJQVMAP(); // init index page's custom scripts
+        Index.initCalendar(); // init index page's custom scripts
+        Index.initCharts(); // init index page's custom scripts
+        Index.initChat();
+        Index.initMiniCharts();
+        Tasks.initDashboardWidget();
+    });
+</script>
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
